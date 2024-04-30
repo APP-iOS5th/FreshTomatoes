743c093bbde201cac869799f36754a977ea02494
avan31017 <avan31017@gmail.com>
2024-05-01 00:43:20 +0900

    [HomeView] Add Api & now-playing movies card

---
+++ FreshTomato/View/HomeViewComponents/NowShowingCardView.swift
@@ -0,0 +1,18 @@
+//
+//  NowShowingCardView.swift
+//  FreshTomato
+//
+//  Created by 조성빈 on 4/30/24.
+//
+
+import SwiftUI
+
+struct NowShowingCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NowShowingCardView()
+}
