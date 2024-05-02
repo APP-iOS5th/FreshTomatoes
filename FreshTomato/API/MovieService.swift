d22dffca8f9b3924ebdc630bb8974451a3d568e5
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-05-02 18:44:55 +0900

    [Feat] Movie API 설정

--- FreshTomato/API/MovieService.swift
+++ FreshTomato/API/MovieService.swift
@@ -7,66 +7,161 @@
 
 import Foundation
 
-@MainActor
-class MovieService: ObservableObject {
-    @Published var movies = [Movie]()
+
+
+import SwiftUI
+import Foundation
+
+
+
+class MovieService {
+
+    static let shared = MovieService()
+    private let baseURL1 = "https://api.themoviedb.org/3/movie/now_playing"
+    private let apiKey = "7f20d823d0156ecf01791f1da2f595d1"
     
-    let url = movieAPI
+    private init() {}
+
     
-    func loadMovie() async {
-        guard let url = URL(string: movieAPI) else {
-            print("Invalid URL")
+    func fetchNowPlayingMovies(language: String, page: Int, region: String, completion: @escaping (Result<[Movie], Error>) -> Void) {
+        let urlString = "\(baseURL1)?api_key=\(apiKey)&language=\(language)&page=\(page)&region=\(region)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
             return
         }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer ACCESS_TOKEN", forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "accept")
         
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-//            print("meta : \(meta)")
-            guard let decodeResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) else {
-                print("decodeResponse Error")
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
                 return
             }
-            movies = decodeResponse.results
-//            print(movies)
-        }catch {
-            print("Invalid data")
-        }
-    }
-    
-    
-    // TMDB 권장 Get
-    enum boxOfficeStatus: String {
-        case nowPlaying = "now_playing"
-        case upComing = "upcoming"
-        case rank = "top_rated"
-    }
-    
-    func load(boxOfficeStatus: boxOfficeStatus) async {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/\(boxOfficeStatus.rawValue)")!
-        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
-        let queryItems: [URLQueryItem] = [
-          URLQueryItem(name: "language", value: "ko-KR"),
-          URLQueryItem(name: "page", value: "1"),
-          URLQueryItem(name: "region", value: "KR"),
-          URLQueryItem(name: "api_key", value: apiKey),
-        ]
-        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
-
-        var request = URLRequest(url: components.url!)
-        request.httpMethod = "GET"
-        request.timeoutInterval = 10
-        request.allHTTPHeaderFields = [
-          "accept": "application/json",
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkyMTlmNjg2NmFkMTBmNWZmZTkwMTFhZDRhZmFkYiIsInN1YiI6IjY2MmZhN2UxOGE4OGIyMDEyN2NlZDFjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sIGK-E0EfADoQWLDscReig-ExB8g2LLchGRH6AGVtRs"
-        ]
-
-        do {
-            let (data, _) = try await URLSession.shared.data(for: request)
-            print(String(decoding: data, as: UTF8.self))
-        } catch {
             
-        }
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NSError(domain: "Server Error", code: 0, userInfo: nil)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No Data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let decodedResponse = try JSONDecoder().decode(MovieResponse.self, from: data)
+                completion(.success(decodedResponse.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 
+    
+    func fetchMultiplePages(pages: [Int], language: String, region: String, completion: @escaping (Result<[Movie], Error>) -> Void) {
+        let dispatchGroup = DispatchGroup()
+        var allMovies: [Movie] = []
+        var anyError: Error?
+
+        for page in pages {
+            dispatchGroup.enter()
+            fetchNowPlayingMovies(language: language, page: page, region: region) { result in
+                switch result {
+                case .success(let movies):
+                    allMovies.append(contentsOf: movies)
+                case .failure(let error):
+                    anyError = error
+                }
+                dispatchGroup.leave()
+            }
+        }
+
+        dispatchGroup.notify(queue: .main) {
+            if let error = anyError {
+                completion(.failure(error))
+            } else {
+                let uniqueMovies = Set(allMovies).sorted(by: { $0.id < $1.id }) // Remove duplicates and sort by id
+                completion(.success(Array(uniqueMovies)))
+            }
+        }
+    }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+//@MainActor
+//class MovieService: ObservableObject {
+//    @Published var movies = [Movie]()
+//    
+//    let url = movieAPI
+//    
+//    func loadMovie() async {
+//        guard let url = URL(string: movieAPI) else {
+//            print("Invalid URL")
+//            return
+//        }
+//        
+//        do {
+//            let (data, _) = try await URLSession.shared.data(from: url)
+////            print("meta : \(meta)")
+//            guard let decodeResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) else {
+//                print("decodeResponse Error")
+//                return
+//            }
+//            movies = decodeResponse.results
+////            print(movies)
+//        }catch {
+//            print("Invalid data")
+//        }
+//    }
+//    
+//    
+//    // TMDB 권장 Get
+//    enum boxOfficeStatus: String {
+//        case nowPlaying = "now_playing"
+//        case upComing = "upcoming"
+//        case rank = "top_rated"
+//    }
+//    
+//    func load(boxOfficeStatus: boxOfficeStatus) async {
+//        let url = URL(string: "https://api.themoviedb.org/3/movie/\(boxOfficeStatus.rawValue)")!
+//        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+//        let queryItems: [URLQueryItem] = [
+//          URLQueryItem(name: "language", value: "ko-KR"),
+//          URLQueryItem(name: "page", value: "1"),
+//          URLQueryItem(name: "region", value: "KR"),
+//          URLQueryItem(name: "api_key", value: apiKey),
+//        ]
+//        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
+//
+//        var request = URLRequest(url: components.url!)
+//        request.httpMethod = "GET"
+//        request.timeoutInterval = 10
+//        request.allHTTPHeaderFields = [
+//          "accept": "application/json",
+//          "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkyMTlmNjg2NmFkMTBmNWZmZTkwMTFhZDRhZmFkYiIsInN1YiI6IjY2MmZhN2UxOGE4OGIyMDEyN2NlZDFjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sIGK-E0EfADoQWLDscReig-ExB8g2LLchGRH6AGVtRs"
+//        ]
+//
+//        do {
+//            let (data, _) = try await URLSession.shared.data(for: request)
+//            print(String(decoding: data, as: UTF8.self))
+//        } catch {
+//            
+//        }
+//    }
+//
+//}
+//
--- FreshTomato/MyData/MyMovie.swift
+++ FreshTomato/MyData/MyMovie.swift
@@ -5,23 +5,23 @@
 //  Created by Jude Song on 5/2/24.
 //
 
-import Foundation
-
-struct MyMovie: Identifiable, Codable, Hashable {
-    var id: Int
-    var title: String
-    var backdrop_path: String?
-    var overview: String
-    let genre_ids: [Int]
-    let genre_names: String
-    
-    var backdropURL: URL? {
-        guard let backdropPath = backdrop_path else { return nil }
-        return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath)")
-    }
-}
-
-struct MovieResponse: Codable {
-    let results: [MyMovie]
-}
-
+//import Foundation
+//
+//struct MyMovie: Identifiable, Codable, Hashable {
+//    var id: Int
+//    var title: String
+//    var backdrop_path: String?
+//    var overview: String
+//    let genre_ids: [Int]
+//    let genre_names: String
+//    
+//    var backdropURL: URL? {
+//        guard let backdropPath = backdrop_path else { return nil }
+//        return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath)")
+//    }
+//}
+//
+//struct MovieResponse: Codable {
+//    let results: [MyMovie]
+//}
+//
